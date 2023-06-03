7776cee69a12fbb91471f6f9f504797e940ca3c9
Revan Kurniawan <rvnkrwn@gmail.com>
2023-06-03 23:20:42 +0700

    add footer element

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
+  Text,
   theme,
   Container,
+  Link,
 } from '@chakra-ui/react';
 import Navbar from './Navbar';
 import Preloading from './components/Preloading';
 import Home from './Home.js';
 
 function App() {
+  const Year = new Date().getFullYear();
   return (
     <ChakraProvider theme={theme}>
       <Preloading/>
@@ -17,6 +20,13 @@ function App() {
         <Container>
           <Navbar />
           <Home />
+          <footer>
+          <Box padding={2} backgroundColor={"whiteAlpha.200"} marginTop={2}>
+            <Text fontSize={14}>Copyright © {Year}</Text>
+            <Text fontSize={12}>Build with ❤️ by <Link target='_blank' href="https://www.linkedin.com/in/rvnkrwn">rvnkrwn</Link></Text>
+          </Box>
+            
+          </footer>
         </Container>
       </Box>
     </ChakraProvider>
